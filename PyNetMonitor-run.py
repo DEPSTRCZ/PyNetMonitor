a62a51bb5d27dbe64d7876ab26a28fb463449e70
DEPSTRCZ <jiri.edelmann@gmail.com>
2023-01-29 20:22:04 +0100

    Working version

--- PyNetMonitor-run.py
+++ PyNetMonitor-run.py
@@ -1,7 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from termcolor import cprint
 import subprocess
-import json
 import csv
 import os
 
@@ -10,42 +9,38 @@ cprint("   PyNetMonitor","blue")
 cprint("╚                ╝","cyan")
 cprint("Tool to long term capture your network speed into csv file. Built around LibreSpeed-CLI","dark_grey")
 cprint("● LibreSpeed Organization: https://github.com/librespeed","dark_grey")
-cprint("● Info:\nThis tool is built around the LibreSpeed-CLI!\nUses their latest release depending on the target system.\nAll ","dark_grey")
-cprint("")
+cprint("● LibreSpeed-CLI repo & Source Code: https://github.com/librespeed/speedtest-cli","dark_grey")
+cprint("● LibreSpeed-CLI Licence: https://github.com/librespeed/speedtest-cli/blob/master/LICENSE","dark_grey")
+cprint("● Notice:\n   This tool is built around the LibreSpeed-CLI!\n   Uses their latest release depending on the target system.\n   The LibreSpeed-CLI is not mine and it is a program developed by LibreSpeed.\n   This is only Tool/Script to capture the output of it in interval to csv file.","dark_grey")
+cprint("Monitor Started..","green")
+fields = ["Year","Date","Time","Ping","Jitter","Download","Upload","Server","Country"]
 def SpeedTestInterval():
-    print("1")
-fields = ["Year","Date","Time","Ping","Jitter","Download","Upload","Server,Country"]
-rows = []
-if not os.path.isfile("SpeedTest.csv"):
-    with open("SpeedTest.csv","x") as csvfile:
-        writer = csv.writer(csvfile)
-        for row in writer:
+    if not os.path.isfile("SpeedTest.csv"):
+        with open("SpeedTest.csv","a", newline="") as csvfile:
+            writer = csv.writer(csvfile)
             writer.writerow(fields)
 
-run = subprocess.run(["librespeed-cli","--csv"],capture_output=True)
+    run = subprocess.run(["librespeed-cli","--csv"],capture_output=True)
 
-result = list(str(run.stdout.decode()).split(","))
-if (run.stderr): 
+    result = list(str(run.stdout.decode()).split(","))
+    if (run.stderr):
+        print("error") 
     
-server = result[3]
-country = result[1][1:]
-ping = result[4]
-jitter = result[5]
-download = result[6]
-upload = result[7]
-year = result[0][:4]
-date = f"{result[0][8:10]}.{result[0][5:7]}"
-time = result[0][11:16]
-# Print the JSON object
-print(country)
+    server = result[3]
+    country = result[1][1:]
+    ping = result[4]
+    jitter = result[5]
+    download = result[6]
+    upload = result[7]
+    year = str(result[0][:4])
+    date = f"{result[0][8:10]}.{result[0][5:7]}"
+    time = result[0][11:16]
 
+    rows = [year,date,time,ping,jitter,download,upload,server,country]
+    with open("SpeedTest.csv","a", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(rows)
 
-with open("SpeedTest.csv","r") as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        rows.append(row)
-rows.append([year,date,time,ping,jitter,download,upload,server,country])
-with open("SpeedTest.csv","w") as csvfile:
-    writer = csv.writer(csvfile) # 4. write the header
-    writer.writerows(rows)
-print(rows)
+scheduler = BlockingScheduler()
+scheduler.add_job(SpeedTestInterval, "interval", hours=1)
+scheduler.start()
